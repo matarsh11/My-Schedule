d5ed203fbceb9038d879d9e9e38826dd7b6ba09c
matarsh11 <54373365+matarsh11@users.noreply.github.com>
2019-09-03 12:55:37 -0500

    Initial commit

---
+++ Source.cpp
@@ -0,0 +1,33 @@
+/*Saleh Almatar
+C++ Fall 2019
+Date: 08 ,27,2019
+Lab#: Lab1
+Lab dessricptio: My week schedule 
+*/
+
+#include <iostream>
+int main()
+{
+	std::cout << "Monday 8:00 College Algebra & Trigonometry 127\n";
+	std::cout << "Monday 9:10 Composition 2 106\n";
+	std::cout << "Monday 12:50 C++ 162\n";
+	std::cout << "Monday 1:40 Classical Physics 212\n";
+	std::cout << "\n";
+	std::cout << "Tuesday 8:00 College Algebra & Trigonometry 127\n";
+	std::cout << "Tuesday 12:50 C++ 162\n";
+	std::cout << "Tuesday 1:40 Classical Physics 212\n";
+	std::cout << "\n";
+	std::cout << "Wednesday 8:00 College Algebra & Trigonometry 127\n";
+	std::cout << "Wednesday 9:10 Composition 2 106\n";
+	std::cout << "Wednesday 12:50 C++ 162\n";
+	std::cout << "Wednesday 1:40 Classical Physics 212\n";
+	std::cout << "\n";
+	std::cout << "Thursday 8:00 College Algebra & Trigonometry 127\n";
+	std::cout << "Thursday 12:50 C++ 162\n";
+	std::cout << "Thursday 1:40 Classical Physics 212\n";
+	std::cout << "\n";
+	std::cout << "Friday 8:00 College Algebra & Trigonometry 127\n";
+	std::cout << "Friday 9:10 Composition 2 106\n";
+	std::cout << "Friday 12:50 C++ 162\n";
+	std::cout << "Friday 1:40 Classical Physics 212\n";
+}
